65365c2b0ef8b0e05088a0823caa4cee00e1a7e4
Mat Dupont <dupont.mat.l@gmail.com>
2018-10-31 12:02:02 -0400

    cleared out site

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,23 +8,13 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
+          Mat Dupont
         </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/matldupont"
           target="_blank"
           rel="noopener noreferrer"
         >
